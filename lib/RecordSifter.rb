fe8a1ffd0ed253b67321cd0e853cb82530b79cd5
Glenn Cullen <glennncullen@gmail.com>
2018-04-04 15:20:27 +0100

    functionality

--- lib/RecordSifter.rb
+++ lib/RecordSifter.rb
@@ -1,5 +1,29 @@
 require "RecordSifter/version"
 
 module RecordSifter
-  # Your code goes here...
+  class Sift
+    def self.sift_record_single_condition(record, column_name, match)
+      records_to_return = []
+      record.all.each do |r|
+        records_to_return << r if r[column_name] == match
+      end
+      records_to_return
+    end
+
+    def sift_record_double_conditional(record, col1, col2, match1, match2)
+      records_to_return = []
+      record.all.each do |r|
+        records_to_return << r if (r[col1] == match1) && (r[col2] == match2)
+      end
+      records_to_return
+    end
+
+    def sift_record_single_conditional_future_query(record, column_name, match, future_col)
+      records_to_return = []
+      record.all.each do |r|
+        records_to_return << r if r[column_name] == match && r[future_col].future?
+      end
+      records_to_return
+    end
+  end
 end
